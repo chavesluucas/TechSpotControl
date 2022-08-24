48a9fde75e281282fba9cc2e00ebbfb86897638d
Lucas <luucas.chaves@gmail.com>
2022-08-24 10:51:07 -0300

    First Commit, create procject

---
+++ src/test/java/tech/lucaschaves/application/ParkingControlApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.lucaschaves.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
